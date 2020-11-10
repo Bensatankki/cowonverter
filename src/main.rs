3f2c343c5ebc885e71bca1ca6d6c56ff44c0cb1e
Bensatankki <axu.hartikainen@gmail.com>
2020-11-10 21:18:47 +0200

    funi error message

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn main() {
     let args = Cli::from_args();
 
     let content = std::fs::read_to_string(&args.path)
-    .expect("file could not be opened");
+    .expect("OOPSIE WOOPSIE!! Uwu We made a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!");
 
     let result:String = content.chars()
     .map(|x| match x {
