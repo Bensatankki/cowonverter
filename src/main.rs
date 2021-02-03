ba743d6c5d58d3e41bd9ee9880026f2f1faa6c6a
Bensatankki <axu.hartikainen@gmail.com>
2021-02-03 16:26:56 +0200

    massive refactoring to make this work as a library

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
-/*
- * TODO use BufReader??
- * TODO nice error handling
- * TODO get a nice pair of programming socks to boost programming skill up to 4x
- */
 
+#[allow(dead_code)]
 fn main() {
-    pub fn convert(text: &str ) -> String {
 
-        let result: String = text.chars()
-        .map(|x| match x {
-            'l' => 'w',
-            'L' => 'W',
-            'r' => 'w',
-            'R' => 'W',
-            _  => x
-        }).collect();
-        result
+    mod converter {
+        
+        #[allow(dead_code)]
+        pub fn convert(text: &str ) -> String {
+
+            let result: String = text.chars()
+            .map(|x| match x {
+                'l' => 'w',
+                'L' => 'W',
+                'r' => 'w',
+                'R' => 'W',
+                _  => x
+            }).collect();
+            result
+        }
     }
 }
