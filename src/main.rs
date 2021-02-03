fb777bac6778b9183d5ae0777184a59c6245b033
Bensatankki <axu.hartikainen@gmail.com>
2021-02-03 16:36:40 +0200

    update version and fix maybe??

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
+fn main(){
+    
+}
+mod converter {
+    
+    #[allow(dead_code)]
+    pub fn convert(text: &str ) -> String {
 
-#[allow(dead_code)]
-fn main() {
-
-    mod converter {
-        
-        #[allow(dead_code)]
-        pub fn convert(text: &str ) -> String {
-
-            let result: String = text.chars()
-            .map(|x| match x {
-                'l' => 'w',
-                'L' => 'W',
-                'r' => 'w',
-                'R' => 'W',
-                _  => x
-            }).collect();
-            result
-        }
+        let result: String = text.chars()
+        .map(|x| match x {
+            'l' => 'w',
+            'L' => 'W',
+            'r' => 'w',
+            'R' => 'W',
+            _  => x
+        }).collect();
+        result
     }
 }
+
