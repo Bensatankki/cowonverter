d2236f961963af1b3c38766dd62e19ce27dd7360
Bensatankki <axu.hartikainen@gmail.com>
2021-02-03 16:11:04 +0200

    massive refactoring to make this work as a library

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,17 @@
  * TODO get a nice pair of programming socks to boost programming skill up to 4x
  */
 
-use structopt::StructOpt;
-
-#[derive(StructOpt)]
-struct Cli {
-    path: std::path::PathBuf,
-}
-
 fn main() {
+    pub fn convert(text: &str ) -> String {
 
-    let args = Cli::from_args();
-
-    let result = std::fs::read_to_string(&args.path);
-    let content = match result {
-        Ok(content) => { content },
-        Err(error) => { panic!("OOPSIE WOOPSIE!! UwU We made a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this! \n error: {}", error); }
-    };
-
-    let result: String = content.chars()
-    .map(|x| match x {
-        'l' => 'w',
-        'L' => 'W',
-        'r' => 'w',
-        'R' => 'W',
-        _  => x
-    }).collect();
-
-println!("{}", result)
+        let result: String = text.chars()
+        .map(|x| match x {
+            'l' => 'w',
+            'L' => 'W',
+            'r' => 'w',
+            'R' => 'W',
+            _  => x
+        }).collect();
+        result
+    }
 }
