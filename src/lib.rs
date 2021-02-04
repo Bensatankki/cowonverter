2f7d392299c4ea4fc2d131768b5aea58afff2782
Bensatankki <axu.hartikainen@gmail.com>
2021-02-04 10:42:24 +0200

    update cargo.lock and made it a little less verbose

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,14 @@
-pub mod converter {    
-    #[allow(dead_code)]
-    pub fn convert(text: &str ) -> String {
+pub mod converter {}
+#[allow(dead_code)]
+pub fn convert(text: &str ) -> String {
 
-        let result: String = text.chars()
-        .map(|x| match x {
-            'l' => 'w',
-            'L' => 'W',
-            'r' => 'w',
-            'R' => 'W',
-            _  => x
-        }).collect();
-        result
-    }
+    let result: String = text.chars()
+    .map(|x| match x {
+        'l' => 'w',
+        'L' => 'W',
+        'r' => 'w',
+        'R' => 'W',
+        _  => x
+    }).collect();
+    result
 }
